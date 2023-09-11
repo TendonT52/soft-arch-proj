770252d9976587281e904361779b19a87dd1a715
TikhampornSky <tikhamporntan@gmail.com>
2023-09-12 00:27:04 +0700

    remove unused struct

--- backend/user-service/domain/email.go
+++ backend/user-service/domain/email.go
@@ -1,12 +1,5 @@
 package domain
 
-// type EmailData struct {
-// 	URL     string
-// 	Subject string
-// 	Name    string
-// 	Email   string
-// }
-
 const (
 	StudentConfirmEmail = "student_confirm_email"
 	CompanyApproveEmail = "company_approve_email"
