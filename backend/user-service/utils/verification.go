fbf7cf79c610ef30b228e5a9d4aa6f55459de303
TikhampornSky <tikhamporntan@gmail.com>
2023-11-10 22:43:34 +0700

    custom encode

--- backend/user-service/utils/verification.go
+++ backend/user-service/utils/verification.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/subtle"
 	"encoding/base64"
+	"regexp"
 	"strconv"
 
 	"github.com/TikhampornSky/go-auth-verifiedMail/config"
@@ -13,7 +14,7 @@ func Encode(code string, current_time int64) string {
 	config, _ := config.LoadConfig("..")
 	passwordWithPepper := append([]byte(code), []byte(config.EmailCode)...)
 	hashedPassword := argon2.IDKey(passwordWithPepper, []byte(strconv.FormatInt(current_time, 10)), argon2Time, argon2Memory, argon2Threads, argon2KeyLen)
-	b64Hash := base64.RawStdEncoding.EncodeToString(hashedPassword)
+	b64Hash := customBase64EncodeToString(hashedPassword)
 	return b64Hash
 }
 
@@ -23,4 +24,16 @@ func Compare(sid string, current_time int64, candidateCode string) bool {
 		return false
 	}
 	return true
+}
+
+func customBase64EncodeToString(input []byte) string {
+	const customBase64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	encoded := make([]byte, base64.RawStdEncoding.EncodedLen(len(input)))
+
+	base64.RawStdEncoding.Encode(encoded, input)
+
+	regExp := regexp.MustCompile("[^a-zA-Z0-9]")
+	result := regExp.ReplaceAllString(string(encoded), "")
+
+	return result
 }
