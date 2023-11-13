6ce250a4f00c2ae1693608f41b38d19cf9bfba8f
Thuwa Ngeonsalung <pai.tuva@gmail.com>
2023-11-13 21:32:22 +0700

    Verify Email text fix
    Failed to verify email text minor layout fix

--- frontend/src/app/(main)/verifyemail/[id]/[...code]/page.tsx
+++ frontend/src/app/(main)/verifyemail/[id]/[...code]/page.tsx
@@ -24,8 +24,10 @@ export default async function Page({
       <div className="flex justify-center">
         <div className="flex h-[440px] w-[440px] flex-col items-center justify-center rounded-lg bg-primary">
           <div className=" m-5 flex h-[400px] w-[400px] items-center justify-center bg-white text-xl">
-            <h1 className="text-xl text-destructive">Failed to verify email</h1>
-            <h2 className="text-xl">Please try again</h2>
+            <div className="text-xl text-destructive">
+              Failed to verify email
+            </div>
+            <div className="text-xl">Please try again</div>
           </div>
         </div>
       </div>
